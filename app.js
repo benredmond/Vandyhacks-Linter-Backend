2b673c8661fcbfbb4cf99ff12b74cb288e3e37c9
wangm18 <martin.wang@vanderbilt.edu>
2018-11-03 14:26:27 -0500

    app,js now makes sure that the code compiles before running analysis, added console logs to make clear what's going on

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
-const { exec } = require('child_process');
+const {exec} = require('child_process');
 const parse = require('./parseAndMap');
 
 let lineToMessage = {};
 let typeToCount = {error: 0, warning: 0, style: 0};
+
 function main(callback) {
-    exec('./cpp/Scripting main', (err, stdout, stderr) => {
+    exec('./cpp/CompileCheck main', (err, stdout, stderr) => {
+
         if (err) {
             // node couldn't execute the command
             console.log("BAD");
             return;
         }
 
-        // if (stdout.toString().contains("EXIT CODE: 0")) {
-        //     console.log("successful");
-        // }
-
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-        parse.parse(lineToMessage, typeToCount, stdout);
-        console.log("line to message mapping:");
-        console.log(lineToMessage);
-        console.log("type to count mapping: ");
-        console.log (typeToCount);
-        callback(stdout);
-
+        let errorCode = stdout;
+        console.log("YOUR CODE FINISHED WITH " + stdout);
+        if (errorCode.includes("EXIT CODE: 0")) {
+            exec('./cpp/Scripting main', (err, stdout, stderr) => {
+                console.log("\n" + "Grading...");
+                console.log(stdout);
+                console.log("the errors maps read:");
+                parse.parse(lineToMessage, typeToCount, stdout);
+                console.log("line to message mapping:");
+                console.log(lineToMessage);
+                console.log("type to count mapping: ");
+                console.log(typeToCount);
+                console.log("\nYour program output: \n");
+                callback(stdout);
+            })
+        }
+        else {
+            console.log("Process must finish exit code 0 before receiving a grade!");
+        }
     });
 }
+
 
 main(() => {
     exec('./cpp/main.out', (err, stdout, stderr) => {
         console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
+        //console.log(`stderr: ${stderr}`);
     });
 });
 
