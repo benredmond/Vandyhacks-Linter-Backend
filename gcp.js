f9eef6845d2e1f6ea420f7422aba6cc3f31d6c56
benredmond <bjr0206@gmail.com>
2018-11-03 10:31:58 -0500

    google cloud vision

--- gcp.js
+++ gcp.js
@@ -1,24 +1,28 @@
-const {Storage} = require('@google-cloud/storage');
+const vision = require('@google-cloud/vision').v1p3beta1;
+const fs = require('fs');
 
 // Creates a client
-const storage = new Storage();
+const client = new vision.ImageAnnotatorClient();
 
 /**
- * TODO(developer): Uncomment the following lines before running the sample.
+ * TODO(developer): Uncomment the following line before running the sample.
  */
-const bucketName = 'Name of a bucket, e.g. my-bucket';
-const filename = 'Local file to upload, e.g. ./local/path/to/file.txt';
+const fileName = `./testimg.jpg`;
 
-// Uploads a local file to the bucket
-await storage.bucket(bucketName).upload(filename, {
-    // Support for HTTP requests made with `Accept-Encoding: gzip`
-    gzip: true,
-    metadata: {
-        // Enable long-lived HTTP caching headers
-        // Use only if the contents of the file will never change
-        // (If the contents will change, use cacheControl: 'no-cache')
-        cacheControl: 'public, max-age=31536000',
+const request = {
+    image: {
+        content: fs.readFileSync(fileName),
     },
-});
-
-console.log(`${filename} uploaded to ${bucketName}.`);
+    feature: {
+        languageHints: ['en-t-i0-handwrit'],
+    },
+};
+client
+    .documentTextDetection(request)
+    .then(results => {
+        const fullTextAnnotation = results[0].fullTextAnnotation;
+        console.log(`Full text: ${fullTextAnnotation.text}`);
+    })
+    .catch(err => {
+        console.error('ERROR:', err);
+    });
